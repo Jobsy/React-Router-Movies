436e485ba0a33e7c9eea8a74275a41114699aad1
Oluwajoba <oluwajobabello@gmail.com>
2019-09-17 07:29:06 +0100

    modify line 13 to accept correct id / change line 8 from 'props.match.url' to 'props.match.params.id' to fully display movie details / Movie.js / part-2.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,15 +3,9 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
-  console.log("mmmmm: ", movie)
-  console.log("ppppp: ", props)
-  console.log("kkkkkk: ", props.match.url)
-  console.log("paaaaam: ", props.match.params.id)
-
- 
+   
   useEffect(() => {
-    const id = props.match.url;
-    // const id = props.match.params.id;
+     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
