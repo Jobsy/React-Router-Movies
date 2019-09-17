9c6c5983d4f7394b541fd878be95cd645610759a
Oluwajoba <oluwajobabello@gmail.com>
2019-09-17 16:08:47 +0100

    fix bug 'error: map undefine'/ movie.js / stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  
+  const [movie, setMovie] = useState({stars: []});
+  
   // console.log("llll: ", props)
   useEffect(() => {
     const id = props.match.params.id;
@@ -43,11 +45,11 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-        {/* {stars.map(star => (
+        {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))} */}
+        ))}
       </div>
       <div className="save-button">Save</div>
     </div>
