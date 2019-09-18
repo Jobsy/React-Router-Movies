ba57a3d92f9db3342023bcf4197c7d4a6c2f627e
Oluwajoba <oluwajobabello@gmail.com>
2019-09-18 02:42:22 +0100

    change useEffect from '    const id = props.match.params.id; / Movie.js / stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -16,8 +16,11 @@ const Movie = (props) => {
   // const [movie, setMovie] = useState([]);
 
   console.log("llll: ", movie)
+
+  const id = props.match.params.id;
+
   useEffect(() => {
-    const id = props.match.params.id;
+    // const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -30,7 +33,7 @@ const Movie = (props) => {
         console.error(error);
       });
 
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   const saveMovie = () => {
