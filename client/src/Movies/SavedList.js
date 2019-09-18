a95d621cf6b980510eff71e3dd1d31deb15d5bc2
Oluwajoba <oluwajobabello@gmail.com>
2019-09-18 14:08:49 +0100

    display unique item / SavedList.js / stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,19 +2,42 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 
-const SavedList = props => (
+// const SavedList = props => (
+// //  const uniqueList = new Set(props.list),
 
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`}>
-        <span className="saved-movie">{movie.title}</span>
-      </Link>
-    ))}
-    <div className="home-button">
-      <Link to="/">Home</Link>
+//   <div className="saved-list">
+//     <h3>Saved Movies:</h3>
+    
+//     {props.list.map(movie => (
+//       <Link to={`/movies/${movie.id}`}>
+//         <span className="saved-movie">{movie.title}</span>
+//       </Link>
+//     ))}
+//     <div className="home-button">
+//       <Link to="/">Home</Link>
+//     </div>
+//   </div>
+// );
+
+const SavedList = props => {
+  console.log(props.list)
+   const uniqueList = [...(new Set(props.list))]
+   console.log(uniqueList)
+
+   return  (
+   <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      
+      {uniqueList.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </Link>
+      ))}
+      <div className="home-button">
+        <Link to="/">Home</Link>
+      </div>
     </div>
-  </div>
-);
+    )
+};
 
 export default SavedList;
